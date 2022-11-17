692345d572343b4519e6704afff64bb806a9f37a
sakasikai <tonq_dlut@126.com>
2022-11-17 17:39:42 +0800

    e

--- code/extract_result_mll.py
+++ code/extract_result_mll.py
@@ -8,88 +8,47 @@ c = '../results/batch/' + str(t) + '/'
 
 
 def get_model_params_result(result_path):
-    with open('params_batch_file.txt', 'w') as pf, open('result_batch_file.txt', 'w') as rf:
-        for target_root, result_dirs, _ in os.walk(result_path):  # (root, dirs, files)
-            for result_dir in result_dirs:
-                target_path = target_root + '/' + result_dir
-                print('in ' + target_path + '...')
-                if os.path.exists(target_path + '/Opt_params.txt'):
-                    # recognize result target
-                    # final_results.txt, Opt_params.txt, <model_name>.model
-                    for b in os.walk(target_path):
-                        assert len(b[0]) == 0 and len(b[1]) == 0, 'err'
-                        print('extracting result target...')
-                        for f in b[2]:
-                            if f.rfind('.model') != -1:
-                                model_name = f.split('.'[0])
-                                write_file(pf, model_name)
-                                write_file(rf, model_name)
-                                break
-                        for f in b[2]:
-                            if f == 'Opt_params.txt':
-                                write_file(pf, target_path, f)
-                            elif f == 'final_results.txt':
-                                write_file(rf, target_path, f)
-                            else:
-                                pass
-                else:
-                    pass
+    with open('batch_params.txt', 'w') as pf, open('batch_results.txt', 'w') as rf:
+        for target_root, dirs, files in os.walk(result_path):  # (root, dirs, files)
+            if len(files) == 0:
+                continue
+            assert len(dirs) == 0
+            print('extracting result target ' + target_root + '...')
+            if os.path.exists(target_root + '/Opt_params.txt'):
+                # recognize result target
+                # final_results.txt, Opt_params.txt, <model_name>.model
+                for f in files:
+                    if f.rfind('.model') != -1:
+                        model_name = f.split('.')[0]
+                        write_header(pf, model_name, target_root)
+                        write_header(rf, model_name, target_root)
+                        break
 
+                for f in files:
+                    if f == 'Opt_params.txt':
+                        write_file(pf, target_root, f)
+                    elif f == 'final_results.txt':
+                        write_file(rf, target_root, f)
+                    else:
+                        pass
+
+        print('\nderive batch_results here:')
+        os.system('pwd')
+        os.system('ls batch*.txt')
         print('done.')
 
 
 def write_file(io, target_path, file_name):
     with open(target_path + '/' + file_name) as r:
+        r.readline()
         io.writelines(r.readlines())
     io.writelines('\n')
-        # if os.path.exists(c + dirname + '/param.txt'):
-        #     if os.path.exists(c + dirname + '/cv_model.model'):
-        #         with open(c + dirname + '/final_results.txt') as f, open(c + dirname + '/cv_model.model') as m,\
-        #                 open(c + dirname + '/param.txt') as p, open(c + 'results.txt', 'a') as w:
-        #             lines = f.readlines()
-        #             m_lines = m.readlines()
-        #             p_lines = p.readlines()
-        #             for line in lines[1:6]:
-        #                 dirname += ' ' + line.strip().split(' ')[-1]
-        #             if t.split('/')[1] == 'svm':
-        #                 param = m_lines[0].strip().split(',')
-        #                 dirname += ' ' + param[1] + ' ' + param[3]
-        #             else:
-        #                 param = m_lines[-1].strip().split(',')
-        #                 dirname += ' ' + param[0]
-        #             dirname += ' ' + p_lines[0]
-        #             w.writelines(dirname + '\n')
-        #     else:
-        #         with open(c + dirname + '/cv_eval_results.txt') as f, open(c + dirname + '/param.txt') as p, \
-        #                 open(c + 'results.txt', 'a') as w:
-        #             lines = f.readlines()
-        #             p_lines = p.readlines()
-        #             for line in lines[1:6]:
-        #                 dirname += ' ' + line.strip().split(' ')[-1]
-        #             if t.split('/')[1] == 'rf':
-        #                 dirname += ' ' + 'None'
-        #             dirname += ' ' + p_lines[0]
-        #             w.writelines(dirname + '\n')
-        # else:
-        #     with open(c + dirname + '/cv_eval_results.txt') as f, open(c + dirname + '/cv_model.model') as m,\
-        #             open(c + 'results.txt', 'a') as w:
-        #         lines = f.readlines()
-        #         m_lines = m.readlines()
-        #         for line in lines[1:6]:
-        #             dirname += ' ' + line.strip().split(' ')[-1]
-        #         if t.split('/')[1] == 'svm':
-        #             param = m_lines[0].strip().split(',')
-        #             dirname += ' ' + param[1] + ' ' + param[3]
-        #         else:
-        #             param = m_lines[-1].strip().split(',')
-        #             dirname += ' ' + param[0]
-        #         dirname += ' ' + 'None'
-        #         w.writelines(dirname + '\n')
 
 
-def write_file(io, tag):
+def write_header(io, tag, root):
     io.writelines(''.center(100, '+') + '\n')
-    io.writelines((' ' + tag + ' ').center(100, '+') + '\n')
+    io.writelines(root[len('../results/batch/'):].center(50, ' ').center(100, '+') + '\n')
+    io.writelines(tag.center(20, ' ').center(100, '+') + '\n')
 
 
 get_model_params_result(c)
