a1d0a87ae360d80d15fb19b5209ebba68b12ce08
sakasikai <tonq_dlut@126.com>
2022-12-01 17:12:14 +0800

    e

--- scripts/mll_param_select_template.sh
+++ scripts/mll_param_select_template.sh
@@ -9,26 +9,24 @@ cpu=$6
 # batch run starting
 
 cd code/
-
-# 有些blocked了
-# RF 2
-p_tree=(450)
-# SVM 2
-p_cost=(-5 1 5)
-p_gamma=(0)
-#RAkELo 3
-p_mll_ls=(2 5 1)
-p_mll_mc=(15)
-#RAkELd 4*3
+# RF
+p_tree=(50 601 100)
+# SVM
+p_cost=(-12 12 4)
+p_gamma=(-12 12 4)
+#RAkELo
+p_mll_ls=(2 5)
+p_mll_mc=(3 20 4)
+#RAkELd
 # p_mll_ls
 # MLkNN
 # BRkNNaClassifier
 # BRkNNbClassifier
-p_mll_k=(450)
-p_mll_s=(0.1 1.0 0.4)
+p_mll_k=(30 165 40)
+p_mll_s=(0.1 1.0 0.3)
 # MLARAM
-p_mll_v=(0.0 1.0 0.3)
-p_mll_t=(0.01 1.0 3)
+p_mll_v=(0 1 0.3)
+p_mll_t=(0.01 0.03 0.01)
 
 default_cmd=(-cv 10 -category DNA -seq_file ${seq_files} -label ${labels} -cpu ${cpu} -bp 1 -metric Acc)
 
@@ -89,13 +87,11 @@ function run_ml_methods() {
 
 # tb345 BSLM based on BOW, TF-IDF, TextRank
 # 12 total
- bslm_modes=(BOW TF-IDF TR) # Attention TR
-# lncRNA very slow
-# dna_words=(Kmer RevKmer Mismatch Subsequence)
-dna_words=(Kmer)
+bslm_modes=(BOW TF-IDF TR) # Attention TR
+dna_words=(Kmer RevKmer Mismatch Subsequence)
 for md in ${bslm_modes[*]}; do
   for wd in ${dna_words[*]}; do
-    blm_mode=(-mode ${md} -words ${wd} -word_size 4)
+    blm_mode=(-mode ${md} -words ${wd})
     run_ml_methods ${blm_mode[*]}
   done
 done
@@ -106,7 +102,7 @@ tm_methods=(LSA LDA PLSA)
 sub_methods=(BOW TF-IDF TextRank) # Attention TextRank
 for md in ${tm_methods[*]}; do
   for sub_md in ${sub_methods[*]}; do
-    blm_mode=(-mode TM -method ${md} -in_tm ${sub_md} -words Kmer -word_size 4)
+    blm_mode=(-mode TM -method ${md} -in_tm ${sub_md} -words Kmer)
     run_ml_methods ${blm_mode[*]}
   done
 done
@@ -114,10 +110,10 @@ done
 # tb7 BNLMs based on word embedding
 # 12 total
 we_methods=(word2vec Glove fastText)
-we_dna_words=(Kmer)  # bugs found for the other two words in BioSeq-BLM
+we_dna_words=(Kmer RevKmer)  # bugs found for the other two words in BioSeq-BLM
 for we in ${we_methods[*]}; do
   for wd in ${we_dna_words[*]}; do
-    blm_mode=(-mode WE -method ${we} -words ${wd} -word_size 4)
+    blm_mode=(-mode WE -method ${we} -words ${wd})
     run_ml_methods ${blm_mode[*]}
   done
 done
