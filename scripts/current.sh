7b9604aa8e5aedbac865e99ec7b7b529fbccd4e8
sakasikai <tonq_dlut@126.com>
2022-12-05 14:38:29 +0800

    e

--- scripts/current.sh
+++ scripts/current.sh
@@ -1,19 +1,35 @@
 #!/bin/bash
 
-#data=(~/blm-mll/data/sequences/snoRNA.fasta ~/blm-mll/data/sequences/snoRNA_label.csv)
-data=(~/blm-mll/data/sequences/miRNA.fasta ~/blm-mll/data/sequences/miRNA_label.csv)
 
-# test run
+# 多肽识别
+./scripts/run_polypeptide.sh BR RF br_rf
+exit 0
+./scripts/run_polypeptide.sh BR SVM br_svm
+./scripts/run_polypeptide.sh LP RF lp_rf
+./scripts/run_polypeptide.sh LP SVM lp_svm
+./scripts/run_polypeptide.sh RAkELo RF rakelo_rf
+./scripts/run_polypeptide.sh RAkELo SVM rakelo_svm
+./scripts/run_polypeptide.sh RAkELd RF rakeld_rf
+./scripts/run_polypeptide.sh RAkELd SVM rakeld_svm
+#./scripts/run_polypeptide.sh MLARAM _ mlaram
+./scripts/run_polypeptide.sh MLkNN _ mlknn
+./scripts/run_polypeptide.sh BRkNNaClassifier _ brknna
+./scripts/run_polypeptide.sh BRkNNbClassifier _ brknnb
+
+# 亚细胞定位
+#data=(~/blm-mll/data/sequences/snoRNA.fasta ~/blm-mll/data/sequences/snoRNA_label.csv)
+#data=(~/blm-mll/data/sequences/miRNA.fasta ~/blm-mll/data/sequences/miRNA_label.csv)
+
 # mll_param_select_template
-./scripts/test.sh BR RF br_rf ${data[*]} 5
-./scripts/test.sh BR SVM br_svm ${data[*]} 5
-./scripts/test.sh LP RF lp_rf ${data[*]} 5
-./scripts/test.sh LP SVM lp_svm ${data[*]} 5
-./scripts/test.sh RAkELo RF rakelo_rf ${data[*]} 5
-./scripts/test.sh RAkELo SVM rakelo_svm ${data[*]} 5
-./scripts/test.sh RAkELd RF rakeld_rf ${data[*]} 5
-./scripts/test.sh RAkELd SVM rakeld_svm ${data[*]} 5
-./scripts/test.sh MLARAM _ mlaram ${data[*]} 5
-./scripts/test.sh MLkNN _ mlknn ${data[*]} 5
-./scripts/test.sh BRkNNaClassifier _ brknna ${data[*]} 5
-./scripts/test.sh BRkNNbClassifier _ brknnb ${data[*]} 5
+#./scripts/test.sh BR RF br_rf ${data[*]} 5
+#./scripts/test.sh BR SVM br_svm ${data[*]} 5
+#./scripts/test.sh LP RF lp_rf ${data[*]} 5
+#./scripts/test.sh LP SVM lp_svm ${data[*]} 5
+#./scripts/test.sh RAkELo RF rakelo_rf ${data[*]} 5
+#./scripts/test.sh RAkELo SVM rakelo_svm ${data[*]} 5
+#./scripts/test.sh RAkELd RF rakeld_rf ${data[*]} 5
+#./scripts/test.sh RAkELd SVM rakeld_svm ${data[*]} 5
+#./scripts/test.sh MLARAM _ mlaram ${data[*]} 5
+#./scripts/test.sh MLkNN _ mlknn ${data[*]} 5
+#./scripts/test.sh BRkNNaClassifier _ brknna ${data[*]} 5
+#./scripts/test.sh BRkNNbClassifier _ brknnb ${data[*]} 5
---
+++ scripts/run_polypeptide.sh
@@ -0,0 +1,139 @@
+#!/bin/bash
+mll=$1
+ml=$2
+out_res=$3
+
+# batch run starting
+cd code/
+
+# data
+cpu=5
+data=(~/blm-mll/data/polypeptide/train/seqs.fasta ~/blm-mll/data/polypeptide/val/seqs.fasta)
+label=(~/blm-mll/data/polypeptide/train/labels.csv ~/blm-mll/data/polypeptide/val/labels.csv)
+ind_data=~/blm-mll/data/polypeptide/test/seqs.fasta
+ind_label=~/blm-mll/data/polypeptide/test/labels.csv
+
+# RF
+p_tree=(450 601 150)
+# SVM
+p_cost=(-5 6 5)
+p_gamma=(0)
+#RAkELo
+p_mll_ls=(5 11 5)
+p_mll_mc=(15)
+# MLkNN, BRkNNaClassifier, BRkNNbClassifier
+p_mll_k=(450)
+p_mll_s=(0.1 1.0 0.4)
+# MLARAM
+#p_mll_v=(0.0 1.0 0.3)
+#p_mll_t=(0.01 1.0 3)
+
+default_cmd=(-cv 10 -category Protein -cpu ${cpu} -bp 1 -metric Acc -mix_mode as_dna\
+ -seq_file ${data[*]} -label ${label[*]} -ind_seq_file ${ind_data} -ind_label_file ${ind_label})
+
+function run_ml_methods() {
+  echo "python BioSeq-BLM_Seq_mllearn.py $* -mll ${mll} -ml ${ml} ${default_cmd[*]}\
+       -tree ${p_tree[*]}"; exit 0
+  if [[ ${mll} = "BR" ||  ${mll} = "LP" ]]; then
+    if [ ${ml} = "RF" ]; then
+      python BioSeq-BLM_Seq_mllearn.py $* -mll ${mll} -ml ${ml} ${default_cmd[*]}\
+       -tree ${p_tree[*]}
+    elif [ ${ml} = "SVM" ]; then
+      python BioSeq-BLM_Seq_mllearn.py $* -mll ${mll} -ml ${ml} ${default_cmd[*]}\
+       -cost ${p_cost[*]} -gamma ${p_gamma[*]}
+    else
+      exit 1
+    fi
+
+  elif [ ${mll} = "RAkELo" ]; then
+    if [ ${ml} = "RF" ]; then
+      python BioSeq-BLM_Seq_mllearn.py $* -mll ${mll} -ml ${ml} ${default_cmd[*]}\
+       -mll_ls ${p_mll_ls[*]} -mll_mc ${p_mll_mc[*]} -tree ${p_tree[*]}
+    elif [ ${ml} = "SVM" ]; then
+      python BioSeq-BLM_Seq_mllearn.py $* -mll ${mll} -ml ${ml} ${default_cmd[*]}\
+       -mll_ls ${p_mll_ls[*]} -mll_mc ${p_mll_mc[*]} -cost ${p_cost[*]} -gamma ${p_gamma[*]}
+    else
+      exit 1
+    fi
+
+  elif [ ${mll} = "RAkELd" ]; then
+    if [ ${ml} = "RF" ]; then
+      python BioSeq-BLM_Seq_mllearn.py $* -mll ${mll} -ml ${ml} ${default_cmd[*]}\
+       -mll_ls ${p_mll_ls[*]} -tree ${p_tree[*]}
+    elif [ ${ml} = "SVM" ]; then
+      python BioSeq-BLM_Seq_mllearn.py $* -mll ${mll} -ml ${ml} ${default_cmd[*]}\
+       -mll_ls ${p_mll_ls[*]} -cost ${p_cost[*]} -gamma ${p_gamma[*]}
+    else
+      exit 1
+    fi
+
+  elif [ ${mll} = "MLARAM" ]; then
+    python BioSeq-BLM_Seq_mllearn.py $* -mll ${mll} ${default_cmd[*]}\
+     -mll_t ${p_mll_t[*]} -mll_v ${p_mll_v[*]}
+
+  elif [ ${mll} = "MLkNN" ]; then
+    python BioSeq-BLM_Seq_mllearn.py $* -mll ${mll} ${default_cmd[*]}\
+     -mll_k ${p_mll_k[*]} -mll_s ${p_mll_s[*]}
+
+
+  elif [[ ${mll} = "BRkNNaClassifier" || ${mll} = "BRkNNbClassifier" ]]; then
+    python BioSeq-BLM_Seq_mllearn.py $* -mll ${mll} ${default_cmd[*]}\
+     -mll_k ${p_mll_k[*]}
+
+  else
+    exit 1
+  fi
+
+  return 0
+}
+
+
+# tb345 BSLM based on BOW, TF-IDF, TextRank
+# 12 total
+bslm_modes=(BOW TF-IDF TR) # Attention TR
+# lncRNA very slow
+# dna_words=(Kmer RevKmer Mismatch Subsequence)
+dna_words=(Kmer)
+for md in ${bslm_modes[*]}; do
+  for wd in ${dna_words[*]}; do
+    blm_mode=(-mode ${md} -words ${wd} -word_size 4)
+    run_ml_methods ${blm_mode[*]}
+  done
+done
+
+#tm_methods=(LSA LDA PLSA)
+#sub_methods=(BOW TF-IDF TextRank) # Attention TextRank
+#for md in ${tm_methods[*]}; do
+#  for sub_md in ${sub_methods[*]}; do
+#    blm_mode=(-mode TM -method ${md} -in_tm ${sub_md} -words Kmer -word_size 4)
+#    run_ml_methods ${blm_mode[*]}
+#  done
+#done
+
+#we_methods=(word2vec Glove fastText)
+#we_dna_words=(Kmer)  # bugs found for the other two words in BioSeq-BLM
+#for we in ${we_methods[*]}; do
+#  for wd in ${we_dna_words[*]}; do
+#    blm_mode=(-mode WE -method ${we} -words ${wd} -word_size 4)
+#    run_ml_methods ${blm_mode[*]}
+#  done
+#done
+
+# generate params and evals in code/
+python extract_result_mll.py Seq/Protein "${out_res}"
+
+# place all results int results/out_res/
+cd ../results/
+# 汇总到tmp，由客户命名
+if [ ! -d "tmp" ]; then
+  mkdir "tmp"
+fi
+
+default_dir="tmp/${out_res}"
+# 覆盖结果
+if [ -d ${default_dir} ]; then
+  rm -rf ${default_dir}
+fi
+mv batch ${default_dir}
+
+echo "Done! results can be found in ${default_dir}"
